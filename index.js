1b8e66e9f78aba598d8319f9d514b2bdcccb4e08
infernalslam <infernal-slam@hotmail.com>
2016-05-29 11:09:09 +0700

    test

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 var express = require('express')
 var bodyParser = require('body-parser')
 var request = require('request')
-var io = require('socket.io')
 var app = express()
+var http = require('http').Server(app)
+var io = require('socket.io')(http)
 app.use(bodyParser.json())
 
 var token = 'EAANmE8COUZA0BANTZBWzFamx1ZCDalrInBZBSBc3GCLQGmFCV2Hy1avC2o15Y5wAsUjJcabADTtqnfGzL5H7C5d9w9QydxUs6yROD4OddinGDU5IvhrkqGIbqkB5HPsYBr2zKWsuMvtn17zuoQOrgzKiOouXTIbok3tV7b42TQZDZD'
@@ -17,7 +18,7 @@ app.set('port', (process.env.PORT || 5000))
 
 app.get('/', function (req, res) {
   // res.send('Hello World!')
-  res.redirect('index.html')
+  res.sendfile('index.html')
 })
 
 io.on('connection', function(socket){
